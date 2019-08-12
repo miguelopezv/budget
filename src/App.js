f6c78242151c0e3f97e9f8db5ff00c2c7ca0fb92
Miguel López <miguellopez@vitalware.com>
2019-08-12 18:37:23 -0500

    add conditional for component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { BudgetInput } from './components';
 
 function App() {
+  const [budget, setBudget] = useState(0);
+  const [budgetexist, setBudgetExist] = useState(false);
+
   return (
     <div className="App">
       <header className="App-header">
         <h1>Weekly Budget</h1>
       </header>
       <div className="content main-content">
-        <BudgetInput />
+        {!budgetexist ? (
+          <BudgetInput setBudget={setBudget} setBudgetExist={setBudgetExist} />
+        ) : (
+          <div className="row">
+            <div className="one-half column">
+              <p>Form</p>
+            </div>
+            <div className="one-half column">
+              <p>List</p>
+            </div>
+          </div>
+        )}
       </div>
     </div>
   );
