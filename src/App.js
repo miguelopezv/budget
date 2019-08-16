8d70cd983a475fecbb5e909729df9187cad7fa70
Miguel López <miguellopez@vitalware.com>
2019-08-16 16:48:20 -0500

    add effect and list

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
-import React, { useState } from 'react';
-import { BudgetInput, Form } from './components';
+import React, { useState, useEffect } from 'react';
+import { BudgetInput, Form, ExpensesList } from './components';
 
 function App() {
   const [budget, setBudget] = useState(0);
   const [budgetexist, setBudgetExist] = useState(false);
+  const [item, setItem] = useState({});
+  const [expenses, setExpenses] = useState([]);
+
+  useEffect(() => {
+    if (!Object.keys(item).length) return;
+
+    const expenseArray = [...expenses, item];
+    setExpenses(expenseArray);
+  }, [item]);
 
   return (
-    <div className="App">
+    <div className="App container">
       <header className="App-header">
         <h1>Weekly Budget</h1>
       </header>
@@ -16,10 +25,10 @@ function App() {
         ) : (
           <div className="row">
             <div className="one-half column">
-              <Form />
+              <Form setItem={setItem} />
             </div>
             <div className="one-half column">
-              <p>List</p>
+              <ExpensesList expenses={expenses} />
             </div>
           </div>
         )}
