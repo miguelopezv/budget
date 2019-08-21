6d1963e8df5a55108fd6bc8e8cbdbd247a1b4065
Miguel López <miguellopez@vitalware.com>
2019-08-21 14:53:14 -0500

    comment button

--- src/components/Expense.js
+++ src/components/Expense.js
@@ -1,12 +1,20 @@
 import React from 'react';
 
-const Expense = ({ expense }) => (
-  <li className="expenses">
-    <p>
-      {expense.label}
-      <span className="expense">$ {expense.quantity}</span>
-    </p>
-  </li>
-);
+const Expense = ({ expense }) => {
+  // TODO: remove expense
+  // const clickHandler = id => {};
+
+  return (
+    <li className="expenses">
+      <p>
+        {expense.label}
+        <span className="expense">$ {expense.quantity}</span>
+        {/* <button type="button" onClick={() => clickHandler(expense.id)}>
+          Remove
+        </button> */}
+      </p>
+    </li>
+  );
+};
 
 export default Expense;
