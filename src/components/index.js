d6d59c8277fbee8488c2807480b0f353b39de91c
Miguel López <miguellopez@vitalware.com>
2019-08-21 10:33:32 -0500

    update barrel

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,6 @@
 import BudgetInput from './BudgetInput';
 import Form from './Form';
 import ExpensesList from './ExpensesList';
+import BudgetController from './BudgetController';
 
-export { BudgetInput, Form, ExpensesList };
+export { BudgetInput, Form, ExpensesList, BudgetController };
